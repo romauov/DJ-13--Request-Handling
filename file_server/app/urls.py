a422431ed8b18896a36b9dad89ab288a35a882fc
Sergei Romanov <l2omanov@ya.ru>
2020-10-16 02:10:10 +0400

    fileserver update

--- file_server/app/urls.py
+++ file_server/app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-
+from app.views import file_content, file_list
 # Определите и зарегистрируйте конвертер для определения даты в урлах и наоборот урла по датам
 
 
 urlpatterns = [
     # Определите схему урлов с привязкой к отображениям .views.file_list и .views.file_content
-    # path(..., name='file_list'),
-    # path(..., name='file_list'),    # задайте необязательный параметр "date"
+    path('', file_list, name='file_list'),
+    path('<date>/', file_list, name='file_list'),    # задайте необязательный параметр "date"
                                       # для детальной информации смотрите HTML-шаблоны в директории templates
-    # path(..., name='file_content'),
+    path('file/<file_name>', file_content, name='file_content'),
 ]
--- file_server/app/views.py
+++ file_server/app/views.py
@@ -1,29 +1,38 @@
 import datetime
-
+from django.http import HttpResponse
 from django.shortcuts import render
+import os
+from django.conf import settings
 
 
-def file_list(request):
+def file_list(request, date=None):
     template_name = 'index.html'
-    
+    my_path = settings.FILES_PATH
+
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
+    files = os.listdir(my_path)
+    files_content = [{'name': file,
+             'ctime': datetime.datetime.fromtimestamp(os.stat(os.path.abspath(f'{my_path}\{file}')).st_ctime), #эту монструозную конструкцию можно как-то упростить?
+             'mtime': datetime.datetime.fromtimestamp(os.stat(os.path.abspath(f'{my_path}\{file}')).st_mtime)} for file in files]
+
     context = {
-        'files': [
-            {'name': 'file_name_1.txt',
-             'ctime': datetime.datetime(2018, 1, 1),
-             'mtime': datetime.datetime(2018, 1, 2)}
-        ],
-        'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
+        'files': files_content,
+        'date': date  # Этот параметр необязательный
     }
 
     return render(request, template_name, context)
 
 
-def file_content(request, name):
+def file_content(request, file_name):
+
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
+    file_path = os.path.abspath(f'files\{file_name}')
+    with open(file_path) as f:
+        f_content = f.read()
+
     return render(
         request,
         'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
+        context={'file_name': file_name, 'file_content': f_content}
     )
 
